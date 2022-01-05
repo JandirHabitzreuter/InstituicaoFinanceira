2a505221ea77201d9e9cc1376f16a292d8b176d1
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2022-01-04 22:19:33 -0300

    Criando rota de create

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 const express = require("express");
+const { json } = require("express/lib/response");
+
+// gerar ID através do uuid. v4 gera com números random
+const {v4: uuidv4} = require("uuid");
 
 const app = express();
+app.use(express.json());
+
+// Enquanto não tiver banco de dados, armazenar em um array
+const customers = [];
+
+// Conta vai conter : 
+// CPF - String
+// name - String
+// id - uuid
+//statement []
+app.post("/account", (request, response)=>{
+  const {cpf, name} = request.body;
+  const id = uuidv4();
+  
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+
+  });
+
+return response.status(201).send(); 
+
+});
 
 app.listen(3333);
 
